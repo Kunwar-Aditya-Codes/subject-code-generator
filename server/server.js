fd90972b4e9473ff3fb96146721e0275e21b884e
Kunwar Aditya <adityakunwar0147@gmail.com>
2022-12-19 08:26:05 +0530

    path.joim

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use(errorHandler);
 app.use('/api/subject', subjectRoute);
 
 const __dirname = path.resolve();
-app.use(express.static(__dirname, '/client/build'));
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
